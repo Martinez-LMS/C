44686d02ae99901af42a1590b2276071eea0e184
Leonardo Martinez dos santos <leo.ooosla@gmail.com>
2023-08-31 19:29:48 -0300

    l
    l

---
+++ ex01.c
@@ -0,0 +1,11 @@
+int main (){
+	int idade = 0;
+	int ano = 1999;
+	printf("\nValor inical de idade: %d. \n", idade);
+	
+	printf("Digite uma idade e um ano:\n");
+	scanf ("%d %d", &idade, &ano);
+	
+	printf ("Idade informada e: %d\n", idade);
+	printf ("ano informada e: %d", ano);
+}
